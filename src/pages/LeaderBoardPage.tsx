9f82499922308d0180f7c1d4dd728f0ddb8681ae
antonvh <antonhaninvl@gmail.com>
2023-02-20 17:57:34 +0300

    my-auto for lead-board

--- src/pages/LeaderBoardPage.tsx
+++ src/pages/LeaderBoardPage.tsx
@@ -4,7 +4,7 @@ import LeaderBoard from '../components/LeaderBoard';
 const LeaderBoardPage: FC = () => (
   <div className="flex justify-center mx-auto h-[calc(100vh-100px)]">
     <div className="flex flex-col max-w-[1024px] w-full">
-      <div className="container mt-4">
+      <div className="container mt-4 mx-auto">
         <LeaderBoard />
       </div>
     </div>
